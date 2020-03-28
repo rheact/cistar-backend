956f51c84c1546f7a51168bd45f3756ce539a719
Sam Kravitz <samkravitz99@gmail.com>
2020-03-28 13:04:06 -0400

    Remove matplotlib

--- hmatrix.py
+++ hmatrix.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt, mpld3
 import pandas as pd
 import numpy as np
 
@@ -55,54 +54,3 @@ def update_h_nums_if_necessary(h_nums, nums):
     for i in range(NUM_HAZARDS):
         if nums[i] > h_nums[i]:
             h_nums[i] = nums[i]
-
-# def max_h_plot(h_ids):
-#     columns = ('Flammability','Reactivity','Skin' 'absorption','Skin' 'Contact','Eye' 'Contact','Respiratory','Carcinogen','Reproductive' 'Hazard','Sensitizer','Other','Ingestion')
-
-#     color_dict = {0:'#10e831',
-#              2:'#f2f745',
-#              3:'#FFA500',
-#              4:'#fc0324'}
-#     df = pd.read_excel("h_matrix.xlsx")
-#     n = len(h_ids)
-#     cell_text = []
-#     colors = []
-#     m_t = []
-#     for i in range(n):
-#         text = [""]*11
-
-#         code = h_ids[i]
-#         print('code: ', code)
-#         if code in list(df['Index']):
-
-#             a= df.loc[df['Index'] == code]
-#             print('a: ', a)
-
-#             b = list(a.iloc[0])
-#             print('b: ', b)
-#             c_n = b[2:]
-#             m_t.append(c_n)
-
-#     try:
-#         m_t = list(np.max(np.array(m_t), axis=0))
-#     except ValueError as e:
-#         print(e)
-
-#     clrs = [color_dict[int(x)] for x in m_t]
-#     colors.append(clrs)
-#     cell_text.append(text)
-
-#     fig, ax = plt.subplots()
-#     ax.axis('tight')
-#     ax.axis('off')
-#     the_table = ax.table(cellText=cell_text,cellColours=colors,
-#                          colLabels=columns,loc='center',colWidths = [0.06, 0.06, 0.06, 0.06, 0.06, 0.06, 0.06, 0.06, 0.06, 0.06, 0.06, 0.06, 0.06] )
-
-#     the_table.auto_set_font_size(False)
-#     the_table.set_fontsize(13)
-
-#     the_table.scale(7,7)
-
-#     plt.savefig('ch.png', bbox_inches='tight')
-#     #plt.show()
-#     return 'ch.png'
