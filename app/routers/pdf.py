c10d2c096e1f516f42bcee64e7708fbb8e1f36ea
Vikrant Gajria <vikrantgajria@gmail.com>
2022-05-12 12:23:40 -0400

    Fix parsing

--- app/routers/pdf.py
+++ app/routers/pdf.py
@@ -29,8 +29,9 @@ async def file_upload(file: UploadFile = File(...)):
     # however does exist in the second database,
     # we'll replace that value in properties with the value from the second database
     for prop in ['boilingPt', 'flashPt', 'autoIgnitionTemp', 'upperExplosionLim', 'lowerExplosionLim']:
+        print(prop)
         if properties.__getattribute__(prop) == 'No data available' and prop in additional_properties and not math.isnan(additional_properties[prop]):
-            properties.__setattr__(prop, additional_properties[prop])
+            properties.__setattr__(prop, str(additional_properties[prop]))
 
     # Close file
     await file.close()
