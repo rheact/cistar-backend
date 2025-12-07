02a0bcba55d8c1ebed584fb7780c0a122e35e0a3
rheactteam <rheactteam@gmail.com>
2025-12-07 11:59:03 +0800

    Add "--no-sandbox" flag

--- services/cameo/crawler.py
+++ services/cameo/crawler.py
@@ -19,7 +19,7 @@ def __get_driver():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--disable-gpu")
-    # chrome_options.add_argument("--no-sandbox") # linux only
+    chrome_options.add_argument("--no-sandbox") # linux only
     driver = webdriver.Chrome(options=chrome_options)
     return driver
 
