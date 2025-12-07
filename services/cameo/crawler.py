7860928ae4c8ab634bbc2882c791f77880e8345c
rheactteam <rheactteam@gmail.com>
2025-12-07 10:25:20 +0800

    Enhance Chrome driver setup in crawler.py
    Added options for Chrome driver configuration including binary location and service path.

--- services/cameo/crawler.py
+++ services/cameo/crawler.py
@@ -1,7 +1,9 @@
 from typing import List
-from selenium import webdriver 
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 import re
+import os
 
 from models.rheact_state import Chemical
 from helpers.errors import ScraperError
@@ -14,13 +16,20 @@ from helpers.errors import ScraperError
 cameo_names = {}
 
 def __get_driver():
-    # These options will speed up futher: 
     chrome_options = Options()
     chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--disable-gpu")
-    # chrome_options.add_argument("--no-sandbox") # linux only
-    driver = webdriver.Chrome(options=chrome_options)
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--no-sandbox")  
+    chrome_options.add_argument("--disable-dev-shm-usage")  
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+
+    service = Service(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
+
+    driver = webdriver.Chrome(
+        service=service,
+        options=chrome_options
+    )
     return driver
 
 def __search_by_cmpd(cmpd_name, driver):
