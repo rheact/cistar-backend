ee03a749f9b39d6a192ad2cdf5ce6257e35a156f
rheactteam <rheactteam@gmail.com>
2025-12-07 14:44:56 +0800

    Updated crawler.py

--- services/cameo/crawler.py
+++ services/cameo/crawler.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException, TimeoutException
 import re
 
 from models.rheact_state import Chemical
